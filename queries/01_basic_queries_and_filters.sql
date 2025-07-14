85866ba6dc966592c0d7d113d62e88e7db9bd2e3
ignaciomajo <ignacio.majoo@gmail.com>
2025-07-14 10:23:21 -0300

    CREATE 01_basic_queries_and_filters.sql

---
+++ queries/01_basic_queries_and_filters.sql
@@ -0,0 +1,10 @@
+/*
+==========================================================================================
+    Script: 01_basic_queries_and_filters.sql
+    Descripción: Consultas básicas con SELECT, filtros con WHERE, operadores lógicos,
+                 manipulación de fechas, y funciones como ROUND y CAST.
+    Base de datos: AdventureWorks2022
+    Autor: Ignacio Majo
+    Fecha de creación: [2025-07-14]
+==========================================================================================
+*/
