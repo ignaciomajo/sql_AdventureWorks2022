e24e422cb8ad3474cee81c0f9b770d356381253b
ignaciomajo <ignacio.majoo@gmail.com>
2025-07-14 14:01:21 -0300

    SOLUTIONS queries 03_aggregations_and_subqueries.sql

---
+++ queries/03_aggregations_and_subqueries.sql
@@ -0,0 +1,183 @@
+/*
+==========================================================================================
+    Script: 03_aggregations_and_grouping.sql
+    Descripción: Consultas con funciones de agregación como SUM, AVG, COUNT, y uso de 
+                 GROUP BY y HAVING. Análisis de ventas, productos y facturación.
+    Base de datos: AdventureWorks2022
+    Autor: Ignacio Majo
+    Fecha de creación: [2025-07-14]
+==========================================================================================
+*/
+
+USE AdventureWorks2022;
+
+
+-- 1. Mostrar el promedio del listado de precios de productos.
+--Tablas: Production.Product
+--Campos: ListPrice
+
+SELECT
+	ROUND(AVG(ListPrice),2) AS AveragePrice
+FROM
+	Production.Product;
+
+
+
+
+
+-- 2. Mostrar la cantidad de ventas y el total vendido.
+--Tablas: Sales.SalesOrderDetail
+--Campos: LineTotal
+
+SELECT 
+	COUNT(*) AS SalesQuantity, 
+	CAST(ROUND(SUM(LineTotal), 2) AS DECIMAL(12, 2)) AS SalesTotal
+FROM
+	Sales.SalesOrderDetail;
+
+
+
+
+-- 3. Mostrar los productos y la cantidad total vendida de cada uno de ellos, ordenados por el total vendido.
+--Tablas: Sales.SalesOrderDetail
+
+
+SELECT
+	ProductID, CAST(ROUND(SUM(LineTotal), 2) AS DECIMAL(12, 2)) AS SalesTotal
+FROM
+	Sales.SalesOrderDetail
+GROUP BY
+	ProductID
+ORDER BY
+	SalesTotal DESC;
+
+
+
+-- 5. Mostrar el promedio vendido por factura.
+--Tablas: Sales.SalesOrderDetail
+
+
+SELECT
+	CAST(ROUND(AVG(LineTotal), 2) AS DECIMAL(10, 2)) AS AverageOrder
+FROM
+	Sales.SalesOrderDetail;
+
+
+
+
+-- 6. Mostrar todas las facturas realizadas y el total facturado de cada una de ellas, ordenado por número de factura,
+--    pero sólo de aquellas órdenes que superen un total de $10.000.
+--Tablas: Sales.SalesOrderDetail
+
+
+SELECT
+	SalesOrderID,
+	CAST(ROUND(SUM(LineTotal), 2) AS DECIMAL(12, 2)) AS SalesTotal
+FROM
+	Sales.SalesOrderDetail
+GROUP BY
+	SalesOrderID
+HAVING
+	SUM(LineTotal) > 10000;
+
+
+
+
+-- 7. Mostrar la cantidad de facturas que vendieron más de 20 unidades.
+--Tablas: Sales.SalesOrderDetail
+
+
+SELECT
+	COUNT(*) AS HighQuantitySales
+FROM
+	(SELECT 
+		SalesOrderID
+	 FROM 
+		Sales.SalesOrderDetail
+	 GROUP BY
+		SalesOrderID
+	 HAVING
+		SUM(OrderQty) > 20) AS HighSales;
+	
+
+
+
+-- 8. Mostrar todos los códigos de subcategorías existentes junto con la cantidad para los productos cuyo
+--    precio de lista sea mayor a $70 y el precio promedio sea mayor a $300.
+--Tablas: Production.Product
+
+
+SELECT 
+    ProductSubcategoryID,
+    COUNT(*) AS QtyProducts
+FROM 
+    Production.Product
+WHERE 
+    ListPrice > 70
+GROUP BY 
+    ProductSubcategoryID
+HAVING 
+    AVG(ListPrice) > 300;
+
+
+
+
+-- 9. Mostrar el código de subcategoría y el precio del producto más barato de cada una de ellas.
+--Tablas: Production.Product
+
+
+SELECT
+	ProductSubcategoryID,
+	MIN(ListPrice) AS LowerPriceProduct
+FROM
+	Production.Product
+GROUP BY
+	ProductSubcategoryID;
+
+
+
+-- 10. Mostrar las subcategorías que tienen dos o más productos que cuestan menos de $150.
+--Tablas: Production.Product
+
+SELECT
+	ProductSubcategoryID,
+	NumProducts
+FROM
+	(SELECT
+		ProductSubcategoryID,
+		COUNT(*) AS NumProducts
+	FROM
+		Production.Product
+	WHERE
+		ListPrice < 150
+	GROUP BY
+		ProductSubcategoryID) AS Subcategories
+WHERE
+	NumProducts >= 2;
+
+
+
+-- 11. Mostrar el número de factura, el monto vendido, y al final, totalizar la facturación.
+--Tablas: Sales.SalesOrderDetail
+
+
+SELECT
+	1 as 'Order',
+	CAST(SalesOrderID AS VARCHAR(20)) as SalesOrderID,
+	CAST(ROUND(SUM(LineTotal), 2) AS DECIMAL(15,2)) AS Total
+FROM
+	Sales.SalesOrderDetail
+GROUP BY
+	SalesOrderID
+UNION ALL
+SELECT
+	2 as 'Order',
+	'SalesOrders Total',
+	CAST(ROUND(SUM(LineTotal), 2) AS DECIMAL(15,2))
+FROM
+	Sales.SalesOrderDetail
+
+ORDER BY
+	'Order';
+
+-- NOTA: Se agrega la columna 'Order' para garantizar que el total se encuentre al final.
